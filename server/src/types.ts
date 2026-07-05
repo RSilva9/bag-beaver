950c27fb69e1eda76e26bf1188f7429238d1ea44
rsilva9 <ramasilva909@gmail.com>
2026-07-05 12:19:07 -0300

    feat: inventory manipulation (add items, remove items, drop items, move items between bags, transfer items to other players) + minor adjustments for inventory handling

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,26 +1,16 @@
 import { ServerWebSocket } from "bun";
-import { Player } from "../../shared/src/types";
-
-export interface JoinData {
-    campaignCode: string;
-    playerName: string;
-}
+import { DroppedItem, Player } from "../../shared/src/types";
 
 export interface ConnectedPlayer {
     player: Player;
-    ws: ServerWebSocket<unknown> | null;
-}
-
-export interface CampaignData {
-    code: string;
-    nextPlayerId: number;
-    players: Player[];
+    ws: ServerWebSocket | null;
 }
 
 export interface Campaign {
     code: string;
     nextPlayerId: number;
-    dm: ServerWebSocket<unknown> | null;
+    dm: ServerWebSocket | null;
     players: Map<number, ConnectedPlayer> | null;
+    droppedItems: DroppedItem[];
     isHosted: boolean;
 }
